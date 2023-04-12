a582949eee7124f76fc29ef4f17dec52e748f21b
novilfahlevy <novilfreon@gmail.com>
2023-04-12 15:21:37 +0800

    home page

---
+++ index.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>PERPUS | Aplikasi Perpustakaan Online</title>
+  <meta content="" name="description">
+  <meta content="" name="keywords">
+
+  <!-- Favicons -->
+  <!-- <link href="assets/frontend/img/favicon.png" rel="icon"> -->
+  <!-- <link href="assets/frontend/img/apple-touch-icon.png" rel="apple-touch-icon"> -->
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Roboto:300,300i,400,400i,500,500i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href="assets/frontend/vendor/animate.css/animate.min.css" rel="stylesheet">
+  <link href="assets/frontend/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="assets/frontend/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link href="assets/frontend/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="assets/frontend/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+  <link href="assets/frontend/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+
+  <!-- Template Main CSS File -->
+  <link href="assets/frontend/css/style.css" rel="stylesheet">
+
+  <!-- =======================================================
+  * Template Name: Shuffle
+  * Updated: Mar 10 2023 with Bootstrap v5.2.3
+  * Template URL: https://bootstrapmade.com/bootstrap-3-one-page-template-free-shuffle/
+  * Author: BootstrapMade.com
+  * License: https://bootstrapmade.com/license/
+  ======================================================== -->
+</head>
+
+<body>
+
+  <!-- ======= Hero Section ======= -->
+  <section id="hero">
+    <div class="hero-container">
+      <div id="heroCarousel" class="carousel slide carousel-fade" data-bs-ride="carousel" data-bs-interval="5000">
+        <div class="carousel-inner" role="listbox">
+          <!-- Slide 1 -->
+          <div class="carousel-item active" style="background-image: url(assets/frontend/img/banner.jpg);">
+            <div class="carousel-container">
+              <div class="carousel-content">
+                <h2 class="animate__animated animate__fadeInDown">Selamat datang di <span>PERPUS</span></h2>
+                <p class="animate__animated animate__fadeInUp">Aplikasi perpustakaan online.</p>
+                <a href="login.php" class="btn-get-started animate__animated animate__fadeInUp scrollto">Login</a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section><!-- End Hero -->
+
+  <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+  <!-- Vendor JS Files -->
+  <script src="assets/frontend/vendor/purecounter/purecounter_vanilla.js"></script>
+  <script src="assets/frontend/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="assets/frontend/vendor/glightbox/js/glightbox.min.js"></script>
+  <script src="assets/frontend/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+  <script src="assets/frontend/vendor/swiper/swiper-bundle.min.js"></script>
+  <script src="assets/frontend/vendor/waypoints/noframework.waypoints.js"></script>
+  <script src="assets/frontend/vendor/php-email-form/validate.js"></script>
+
+  <!-- Template Main JS File -->
+  <script src="assets/frontend/js/main.js"></script>
+
+</body>
+
+</html>
